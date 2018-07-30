05049466da67ec834f904abdbecdafe055a3fbde
Kenneth Wong <Kenneth@Kenneths-MacBook-Pro.local>
2018-07-30 05:36:12 -0700

    first commit

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "vrw0YvjCYEsUcN8hAtAeAUUWW"
+consumer_secret = "00ZkunY3RL6eQrkjFXghxT1I067z7PBiGdyYrZqgCkh5mI8uMx"
+access_token = "364222056-VjnnfNKSXVEWSu5p3kQfhMGScaQMmtZxJ2Jbeuwf"
+access_token_secret = "M9sL0bjGIquPRfPrh3UFKOC5RTlawPD15jhoXhre7khhp"
